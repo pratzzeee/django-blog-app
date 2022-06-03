ca7765bcdc2f200547e7882d32f149d67ec621a5
Prathyush Maniyam <prathyushmaniyam@Prathyushs-MacBook-Air.local>
2022-06-03 12:24:02 +0530

    another update

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*f*4r(3&q6#_v*^0&ot4ae^+tn7$cxp1-anay!lb0_$vchsx$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['copyofcsdjango.herokuapp.com']
 
 
 # Application definition
